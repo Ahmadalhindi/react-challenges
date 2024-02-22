b2c1405114110c69f5ba92f35f741ccfd6f87d0d
Ahmad alhindi <ahmadyoussiv@gmail.com>
2024-02-22 16:11:35 +0000

    Rendering Lists Challenge - Creating a child component.

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,38 +1,44 @@
 import React, { Component } from 'react';
 import {savedPosts} from "../posts.json";
 import css from "./css/Content.module.css";
+import PostItem from "./PostItem";
 
 
-class Content extends Component {
+export class Content extends Component {
+    constructor(props) {
+        super(props)
+    }
+    
+    render() {
+        return (
+            <div className={css.Content}>
+                
+                <div className={css.TitleBar}>
+                    <h1>My Photos</h1>
+                </div>
 
-  constructor(props) {
-    super(props)
-  }
-  
-  render() {
-    return (
-      <div className={CSS.Content}>
+                <div className={css.SearchResults}>
 
-        <div className={css.TitleBar}>
-          <h1>My Photos</h1>
-        </div>
+                    {/* Part 1: Creating the map function */}
 
-        <div className={css.SearchResults}>
-          {
-            savedPosts.map((post)=>{
-              return <div className={css.SearchItem} key={post.title}>
-                  <p>{post.title}</p>
-                  <p>{post.name}</p>
-                  <img src={post.image} alt="random"/>
-                  <p>{post.description}</p>
-                  </div>
-            })
-          }
-        </div>
+                        {/* {
+                    savedPosts.map((post)=>{
+                            return <div className={css.SearchItem} key={post.title}>
+                                <p>{post.title}</p>
+                                <p>{post.name}</p>
+                                <img src={post.image} alt="random"/>
+                                <p>{post.description}</p>
+                                </div>
+                        })
+                    } */}
 
-      </div>
-    )
-  }
+
+                    {/* Part 2: Creating a child component */}
+
+                    <PostItem savedPosts={savedPosts} />
+                </div>
+            </div>
+        )
+    }
 }
-
 export default Content
