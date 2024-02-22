aee628f7ed25af685cfed2387a17470ca12f9309
Ahmad alhindi <ahmadyoussiv@gmail.com>
2024-02-22 16:18:34 +0000

    Rendering Lists Challenge - Creating a child component - Extra Tasks: Try Refactoring your code to: Within the PostItem.js map function, try destructuring the posts from posts.json to 4 separate values e.g. name, title, description, and image.

--- my-app/src/components/PostItem.js
+++ my-app/src/components/PostItem.js
@@ -4,11 +4,13 @@ import css from "./css/PostItem.module.css";
 function PostItem(props) {
     return (
         props.savedPosts.map((post)=>{
-            return <div className={css.SearchItem} key={post.title}>
-                <p>{post.title}</p>
-                <p>{post.name}</p>
-                <img src={post.image} alt="random"/>
-                <p>{post.description}</p>
+            // Extra task destructuring 
+            const {title, name, image, description} = post
+            return <div className={css.SearchItem} key={title}>
+                <p>{title}</p>
+                <p>{name}</p>
+                <img src={image} alt="random"/>
+                <p>{description}</p>
                 </div>
         })
     )
